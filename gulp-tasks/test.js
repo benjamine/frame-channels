052457d5d74f46a7b02424de92a4ffc7c2f58c20
Benjamin Eidelman <beneidel@gmail.com>
2014-08-08 02:49:21 -0300

    jshint indentation

--- gulp-tasks/test.js
+++ gulp-tasks/test.js
@@ -4,12 +4,12 @@ var plugins = require('gulp-load-plugins')();
 var plumber = require('./util/plumber').plumber;
 
 gulp.task('test', ['lint'], function () {
-    return gulp.src('./test/index.js')
-        .pipe(plumber())
-        .pipe(plugins.mocha({
-            grep: process.env.FILTER || undefined,
-            reporter: 'spec',
-            growl: true
-        }))
-        .pipe(plumber());
+  return gulp.src('./test/index.js')
+    .pipe(plumber())
+    .pipe(plugins.mocha({
+      grep: process.env.FILTER || undefined,
+      reporter: 'spec',
+      growl: true
+    }))
+    .pipe(plumber());
 });
