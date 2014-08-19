ecb3d0d3aec90528c4029e9cfeba9ff0eb1200ca
Benjamin Eidelman <beneidel@gmail.com>
2014-08-19 15:48:20 -0300

    gulp bumping

--- gulp-tasks/bump.js
+++ gulp-tasks/bump.js
@@ -1,73 +1,28 @@
 var gulp = require('gulp');
-var exec = require('./util/exec');
-
-exec.task('npm-version-patch', [], 'npm version patch');
-exec.task('npm-publish', [], 'npm publish');
+var path = require('path');
+require('shelljs/global');
+/*global config, exec, cd */
+config.fatal = true;
 
 gulp.task('bump', [], function(callback) {
   var packageInfo = require('../package');
-  exec('npm', ['version', 'patch'], function(err){
-    if (err) {
-      callback(err);
-      return;
-    }
-    exec('npm', ['test'], function(err){
-      if (err) {
-        callback(err);
-        return;
-      }
-      exec('git', ['add', '--all', '.'], function(err){
-        if (err) {
-          callback(err);
-          return;
-        }
-        exec('git', ['commit', '--amend', '--no-edit'], function(err){
-          if (err) {
-            callback(err);
-            return;
-          }
-          exec('git', ['push'], function(err){
-            if (err) {
-              callback(err);
-              return;
-            }
-            if (packageInfo.private) {
-              console.log('private package, skipping npm publish');
-              callback();
-            }
-            if (!packageInfo.homepage) {
-              console.log('no package homepage specified, skipping npm publish');
-              callback();
-            }
-            exec('npm', ['publish'], function(err){
-              if (err) {
-                callback(err);
-                return;
-              }
-              callback();
-            });
-          });
-        });
-      });
-    });
-  });
-  /*
-    npm version patch
-    if (browser)
-      gulp bundle
-      amend
-    fi
-    if (pages) {
-      gulp pages
-      if (pages is git subrepo) {
-        cd subrepo
-        git a && commit
-        git push
-        cd ..
-      }
-      amend
-    }
-    git push
-    npm publish
-  */
+  cd(path.join(__dirname, '..'));
+  exec('npm version patch');
+  exec('npm test');
+  if (exec('git status --porcelain ./build')) {
+    // append build output
+    exec('git add --all ./build');
+    exec('git commit --amend --no-edit');
+  }
+  exec('git push');
+  if (packageInfo.private) {
+    console.log('private package, skipping npm publish');
+    callback();
+  }
+  if (!packageInfo.homepage) {
+    console.log('no package homepage specified, skipping npm publish');
+    callback();
+  }
+  exec('npm publish');
+  callback();
 });
