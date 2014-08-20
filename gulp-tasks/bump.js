0b012585662fea944520ce1fd7cb7d8edc714b8c
Benjamin Eidelman <beneidel@gmail.com>
2014-08-20 15:24:58 -0300

    pages folder

--- gulp-tasks/bump.js
+++ gulp-tasks/bump.js
@@ -1,20 +1,49 @@
 var gulp = require('gulp');
 var path = require('path');
 require('shelljs/global');
-/*global config, exec, cd */
+/*global config, exec, cd, test, cp */
 config.fatal = true;
 
 gulp.task('bump', [], function(callback) {
   var packageInfo = require('../package');
   cd(path.join(__dirname, '..'));
+
+  // version bump
   exec('npm version patch');
+
+  // test
   exec('npm test');
   if (exec('git status --porcelain ./build')) {
-    // append build output
+    // update /build
     exec('git add --all ./build');
     exec('git commit --amend --no-edit');
   }
+
+  if (packageInfo.repository && test('-d', './pages')) {
+    // update gh-pages
+    if (!test('-d', './gh-pages')) {
+      exec('git submodule add ' + packageInfo.repository + ' gh-pages');
+    }
+    cd('./gh-pages');
+    if (exec('git rev-parse --abbrev-ref HEAD') !== 'gh-pages') {
+      if (exec('git branch --list gh-pages') !== 'gh-pages') {
+        exec('git checkout --orphan gh-pages');
+      } else {
+        exec('git checkout gh-pages');
+      }
+    }
+    cp('-R', '../pages/*', './');
+    cp('-R', '../build/*', './build');
+    if (exec('git status --porcelain .')) {
+      exec('git add --all .');
+      exec('git commit --no-edit -m "version bump"');
+      exec('git push');
+    }
+    cd('..');
+  }
+
   exec('git push');
+  exec('git push --tags');
   if (packageInfo.private) {
     console.log('private package, skipping npm publish');
     callback();
