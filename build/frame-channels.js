8e8b02c37bb664e5526c0b9588feb43c4a373e7d
Benjamin Eidelman <beneidel@gmail.com>
2014-09-12 10:29:49 -0300

    0.0.63

--- build/frame-channels.js
+++ build/frame-channels.js
@@ -17,7 +17,7 @@ if (inNode) {
 } else {
   // exports only for browser bundle
 	exports.homepage = 'https://github.com/benjamine/frame-channels';
-	exports.version = '0.0.62';
+	exports.version = '0.0.63';
 }
 
 }).call(this,require('_process'))
