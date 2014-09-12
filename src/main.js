4d7174abfffdffe2a9e3e1f329b9d15c9fd01278
Benjamin Eidelman <beneidel@gmail.com>
2014-09-12 12:31:10 -0300

    slush fiberglass update

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
 
+// global exports
+
 var Channel = require('./channel');
 exports.Channel = Channel;
 exports.create = function channel(name, options) {
   return new Channel(name, options);
 };
 
-// detect runtime
-var inNode = typeof process !== 'undefined' && typeof process.execPath === 'string';
-if (inNode) {
+if (process.browser) {
+  // exports only for browser bundle
+  exports.version = '{{package-version}}';
+  exports.homepage = '{{package-homepage}}';
+} else {
   // exports only for node.js
-  var packageInfo = require('../package' + '.json');
+  var packageInfo = require('../pack'+'age.json');
   exports.version = packageInfo.version;
   exports.homepage = packageInfo.homepage;
-} else {
-  // exports only for browser bundle
-	exports.homepage = '{{package-homepage}}';
-	exports.version = '{{package-version}}';
 }
